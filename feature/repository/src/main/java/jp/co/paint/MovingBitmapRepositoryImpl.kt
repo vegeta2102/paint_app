0e0748aba84a8d15cd4289a9db72b08bfc462d53
vegeta2102 <hoangvmh@gmail.com>
2021-07-26 14:57:27 +0900

    Change to singleLiveData

--- feature/repository/src/main/java/jp/co/paint/MovingBitmapRepositoryImpl.kt
+++ feature/repository/src/main/java/jp/co/paint/MovingBitmapRepositoryImpl.kt
@@ -2,13 +2,14 @@ package jp.co.paint
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import jp.co.paint.core.extentions.SingleLiveEvent
 import javax.inject.Inject
 
 /**
  * Created by vegeta on 2021/07/20.
  */
 class MovingBitmapRepositoryImpl @Inject constructor() : MovingBitmapRepository {
-    private val isTouchMutable = MutableLiveData<Boolean>()
+    private val isTouchMutable = SingleLiveEvent<Boolean>()
     override val isTouch: LiveData<Boolean>
         get() = isTouchMutable
 
--- feature/startup/src/main/java/jp/co/paint/startup/StartupViewModel.kt
+++ feature/startup/src/main/java/jp/co/paint/startup/StartupViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import jp.co.paint.core.extentions.SingleLiveEvent
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,7 +14,7 @@ import javax.inject.Inject
  */
 class StartupViewModel @Inject constructor() : ViewModel() {
 
-    private val mutableInitFinished = MutableLiveData<Unit>()
+    private val mutableInitFinished = SingleLiveEvent<Unit>()
     val initFinished: LiveData<Unit>
         get() = mutableInitFinished
 
